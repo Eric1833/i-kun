db648060d1b96bd520dd811ea7ce249f6b4a0a04
jiayi fu <89924263+fff1316169257@users.noreply.github.com>
2022-09-24 21:16:46 -0500

    Update Setting.cs

--- Setting.cs
+++ Setting.cs
@@ -1,4 +1,18 @@
 //quote
+/*
+@Name： Setting
+@Describtion：the script part for the Setting
+@Programmer: Jiayi Fu, Yuyang Fan, Chen Lu, Zhaoyang Xue, Haoxuan Feng
+@Date: 24th September 2022
+        virsion 0.0.0 create code and comments  by Jiayi Fu, Yuyang Fan, Chen Lu, Zhaoyang Xue, Haoxuan Feng
+Part for function
+@Preconditions: None
+@Postconditions: None
+@Error and exception condition values or types that can occur, and their meanings: None
+@Side effects: None
+@Invariants: None
+@known faults: None
+*/
 using System.Collections;   
 using System.Collections.Generic;
 using UnityEngine;//call unity
