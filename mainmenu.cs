a6ba39b33ad877184a6265b4f6f88f67cd322ee0
maomaosa <blessingemao@gmail.com>
2022-09-24 21:12:17 -0500

    Update mainmenu.cs

--- mainmenu.cs
+++ mainmenu.cs
@@ -1,3 +1,22 @@
+/*
+@Name： mainmenu
+@Describtion：the script part for the main menu
+@Programmer: Jiayi Fu, Yuyang Fan, Chen Lu, Zhaoyang Xue, Haoxuan Feng
+@Date: 24th September 2022
+        virsion 0.0.0 create code and comments  by Jiayi Fu, Yuyang Fan, Chen Lu, Zhaoyang Xue, Haoxuan Feng
+
+Part for function
+@Preconditions: None
+@Postconditions: None
+@Error and exception condition values or types that can occur, and their meanings: None
+@Side effects: None
+@Invariants: None
+@known faults: None
+*/
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
